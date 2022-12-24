6f47f2be48b6a7335b96beb608b3711f01ca169f
Matheus Alves <mathalves07.winstore@outlook.com>
2022-12-23 22:12:41 -0300

    Included Entity Framework and Postgresql references, and changed the student example from the controller

--- SmartSchool.API/Controllers/StudentController.cs
+++ SmartSchool.API/Controllers/StudentController.cs
@@ -1,3 +1,4 @@
+using SmartSchool.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SmartSchool.API.Controllers;
@@ -9,5 +10,5 @@ public class StudentController: ControllerBase
     public StudentController() {  }
 
     [HttpGet]
-    public IActionResult Get() => Ok("Alunos=> Marta, Paula, Lucas, Rafaela");
+    public IActionResult Get() => Ok(new Student(Guid.NewGuid(),"Aluno","de Teste","(99)999-999-999"));
 }
