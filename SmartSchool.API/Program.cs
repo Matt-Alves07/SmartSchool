16f1962ab54bcedb248cbbf0bea2fecb6b66d0ce
Matheus Alves <mathalves07.winstore@outlook.com>
2022-12-24 19:08:00 -0300

    Fixed a dependecy injection bug in Program.cs

--- SmartSchool.API/Program.cs
+++ SmartSchool.API/Program.cs
@@ -5,8 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 builder.Services.AddDbContext<DBContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
