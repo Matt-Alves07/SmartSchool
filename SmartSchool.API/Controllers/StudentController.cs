4a2917a046303daf01d53fcaf373d63fd171347e
Matheus Alves <mathalves07.winstore@outlook.com>
2022-12-24 19:00:22 -0300

    Finished creating the endpoints for Students and Teachers

--- SmartSchool.API/Controllers/StudentController.cs
+++ SmartSchool.API/Controllers/StudentController.cs
@@ -1,5 +1,7 @@
 using SmartSchool.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using SmartSchool.API.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace SmartSchool.API.Controllers;
 
@@ -7,8 +9,69 @@ namespace SmartSchool.API.Controllers;
 [Route("[controller]")]
 public class StudentController: ControllerBase
 {
-    public StudentController() {  }
+    private readonly DBContext _context;
+
+    public StudentController(DBContext context)
+    {
+        _context = context;
+    }
 
     [HttpGet]
-    public IActionResult Get() => Ok(new Student(Guid.NewGuid(),"Aluno","de Teste","(99)999-999-999"));
+    public IActionResult Get()
+    {
+        var list = _context.Students?.ToList();
+
+        if (list is null)
+            return NoContent();
+
+        return Ok(list);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(Guid id)
+    {
+        var student = _context.Students?.FirstOrDefault(s => s.Id == id);
+
+        if (student == null)
+            return NotFound();
+
+        return Ok(student);
+    }
+
+    [HttpPost]
+    public IActionResult Post(Student student)
+    {
+        _context.Add(student);
+        _context.SaveChanges();
+
+        return Ok(student);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(Guid id, Student student)
+    {
+        var _student = _context.Students?.AsNoTracking().FirstOrDefault(s => s.Id == id);
+
+        if (_student == null)
+            return NotFound();
+
+        _context.Update(student);
+        _context.SaveChanges();
+
+        return Ok(student);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(Guid id)
+    {
+        var student = _context.Students?.AsNoTracking().FirstOrDefault(s => s.Id == id);
+
+        if (student == null)
+            return BadRequest();
+
+        _context.Remove(student);
+        _context.SaveChanges();
+
+        return NoContent();
+    }
 }
--- SmartSchool.API/Controllers/TeacherController.cs
+++ SmartSchool.API/Controllers/TeacherController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartSchool.API.Data;
+using SmartSchool.API.Models;
 
 namespace SmartSchool.API.Controllers;
 
@@ -6,8 +9,69 @@ namespace SmartSchool.API.Controllers;
 [Route("/[controller]")]
 public class TeacherController: ControllerBase
 {
-    public TeacherController() { }
+    private readonly DBContext _context;
+
+    public TeacherController(DBContext context)
+    {
+        _context = context;
+    }
 
     [HttpGet]
-    public IActionResult Get() => Ok("Teachers=> Leandro, Carlos, Sílvio, Fátima.");
+    public IActionResult Get()
+    {
+        var list = _context.Teachers?.ToList();
+
+        if (list == null)
+            return NoContent();
+
+        return Ok(list);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(Guid id)
+    {
+        var teacher = _context.Teachers?.FirstOrDefault(t => t.Id == id);
+
+        if (teacher == null)
+            return NotFound();
+
+        return Ok(teacher);
+    }
+
+    [HttpPost]
+    public IActionResult Post(Teacher teacher)
+    {
+        _context.Add(teacher);
+        _context.SaveChanges();
+
+        return Ok(teacher);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(Guid id, Teacher teacher)
+    {
+        var _teacher = _context.Teachers?.AsNoTracking().FirstOrDefault(t => t.Id == id);
+
+        if (_teacher == null)
+            return NotFound();
+
+        _context.Update(teacher);
+        _context.SaveChanges();
+
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(Guid id)
+    {
+        var teacher = _context.Teachers?.AsNoTracking().FirstOrDefault(t => t.Id == id);
+
+        if (teacher == null)
+            return NotFound();
+
+        _context.Remove(teacher);
+        _context.SaveChanges();
+
+        return NoContent();
+    }
 }
